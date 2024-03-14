50863cd6bdfb31351e8ee5566b36029acf7b2c8d
EverettKelsey <kelseyj@my.erau.edu>
2024-03-14 16:06:42 -0700

    finished dev

---
+++ Lighting/Assets/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+    
+    public void PreviousScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Lighting/Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        { 
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A)) { 
+            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-1 * Vector3.forward * Time.deltaTime * moveSpeed);
+        }
+    }
+}
